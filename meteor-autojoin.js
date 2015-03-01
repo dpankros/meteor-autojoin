f4b01fda892d751debfa19b5b9f2d80bdef755c7
David Pankros <dpankros@miragy.com>
2015-02-28 21:32:58 -0500

    Added depth to fetch options Added joinDepth to preferences.  Defaults to one level deep.

--- meteor-autojoin.js
+++ meteor-autojoin.js
@@ -22,10 +22,11 @@ var private = private || {};
  * @param {String} prop - the property to expand
  * @param {String} propValue - the Initial value of the property
  * @param {Object} propSchema - the SimpleSchema definition for the property
+ * @param {Number} depth - the maximum depth to expand
  * @return {Object} The exanded property
  */
 
-private.expandProp = function expandProp(prop, propValue, propSchema) {
+private.expandProp = function expandProp(prop, propValue, propSchema, depth) {
   if (!prop || !propValue) {
     return propValue;
   }
@@ -38,7 +39,7 @@ private.expandProp = function expandProp(prop, propValue, propSchema) {
   var query = {};
   query[id_key] = propValue;
   //expand the value
-  return window[collection].findOne(query);
+  return window[collection].findOne(query, {autojoin: {depth: depth - 1}});
 }
 
 
@@ -46,16 +47,17 @@ private.expandProp = function expandProp(prop, propValue, propSchema) {
  * Expands a whole document
  * @param {Object} doc - the document
  * @param {Object} docSchema - the SimpleSchema defintion for the document
+ * @param {Number} depth - the maximum depth to expand
  * @return {*} The expanded document
  */
-private.expandDoc = function expandDoc(doc, docSchema) {
-  if (!doc || !docSchema) {
+private.expandDoc = function expandDoc(doc, docSchema, depth) {
+  if (!doc || !docSchema || depth === 0) {
     return doc;
   }
   var prop;
   for (prop in doc) {
     var propSchema = docSchema.getDefinition(prop);
-    doc[prop] = private.expandProp(prop, doc[prop], propSchema);
+    doc[prop] = private.expandProp(prop, doc[prop], propSchema, depth);
   }
   return doc;
 }
@@ -99,17 +101,24 @@ var __origMongoCollectionFindOne = Mongo.Collection.prototype.findOne;
 Mongo.Collection.prototype.findOne = function findOneWithJoin(sel, opt) {
   var self = this;
   var doJoin = AutoJoin.prefs.joinWhenSpecified;
+  var depth = AutoJoin.prefs.joinDepth;
 
   if (opt && opt.hasOwnProperty('autojoin')) {
-    doJoin = opt['autojoin'];
+    var aj = opt['autojoin'];
+    if (aj.hasOwnProperty('automatic')) {
+      doJoin = aj['automatic'];
+    }
+    if (aj.hasOwnProperty('depth')) {
+      depth = aj['depth'];
+    }
     delete opt['autojoin'];
   }
 
   var result = __origMongoCollectionFindOne.apply(this, arguments);
   var ss = this.simpleSchema();
 
-  if (doJoin && ss) {
-    result = AutoJoin.private.expandDoc(result, ss);
+  if (doJoin && ss ) {
+    result = AutoJoin.private.expandDoc(result, ss, depth);
   }
 
   return result;
@@ -126,17 +135,26 @@ Mongo.Collection.prototype.findOne = function findOneWithJoin(sel, opt) {
 Mongo.Collection.prototype.find = function findWithJoin(sel, opt) {
   var self = this;
   var doJoin = AutoJoin.prefs.joinWhenSpecified;
+  var depth = AutoJoin.prefs.joinDepth;
 
   if (opt && opt.hasOwnProperty('autojoin')) {
-    doJoin = opt['autojoin'];
+    var aj = opt['autojoin'];
+    if (aj.hasOwnProperty('automatic')) {
+      doJoin = aj['automatic'];
+    }
+    if (aj.hasOwnProperty('depth')) {
+      depth = aj['depth'];
+    }
     delete opt['autojoin'];
   }
+
 
   var result = __origMongoCollectionFind.apply(this, arguments);
 
   if (doJoin) {
     //add the schema to the cursor for later access, if needed
     result.__afSchema = this.simpleSchema();
+    result.__ajDepth = depth;
   }
   return result;
 };
@@ -160,9 +178,10 @@ Mongo.Cursor.fetch = function fetchWithJoin() {
   var newArray = [];
   var i;
   var doc;
+  var depth = this.__ajDepth;
   for (i = 0; i < result.length; i++) {
     doc = result[i];
-    newArray.push(AutoJoin.private.expandDoc(doc, this.__afSchema));
+    newArray.push(AutoJoin.private.expandDoc(doc, this.__afSchema, depth));
   }
   return newArray;
 };
@@ -180,9 +199,10 @@ Mongo.Cursor.forEach = function forEachWithjoin(callback, opt_thisArg) {
   if (!this.__afSchema) {
     return __origMongoCursorForEach(arguments, this);
   }
-
+  var depth = this.__ajDepth;
   var wrappedCallback = function(obj, ndx, cursor) {
-    obj = AutoJoin.private.expandDoc(obj, this.__afSchema);
+    var depth = depth.copy();
+    obj = AutoJoin.private.expandDoc(obj, this.__afSchema, depth);
     return callback.apply(obj, this);
   };
   arguments[0] = wrappedCallback;
@@ -202,9 +222,10 @@ Mongo.Cursor.map = function mapWithJoin(callback, opt_thisArg) {
   if (!this.__afSchema) {
     return __origMongoCursorMap(arguments, this);
   }
-
+  var depth = this.__ajDepth;
   var wrappedCallback = function(obj, ndx, cursor) {
-    obj = AutoJoin.private.expandDoc(obj, this.__afSchema);
+    var depth = depth.copy();
+    obj = AutoJoin.private.expandDoc(obj, this.__afSchema, depth);
     return callback.apply(obj, this);
   };
   arguments[0] = wrappedCallback;
