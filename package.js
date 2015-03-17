4ca0bb91222a1df98ccecf638ecd04b80069c26c
David Pankros <dpankros@miragy.com>
2015-03-17 08:24:30 -0400

    Bump version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dpankros:autojoin',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Allows simple joins to be defined from within a SimpleSchema',
   // URL to the Git repository containing the source code for this package.
